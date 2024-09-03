b7c367cc29f64ece9cc7ee2f6b70b68868839660
UnLakhs <apostolos052@gmail.com>
2024-09-03 14:18:18 +0300

    carousel progress

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,11 +1,22 @@
 function initializeCarousel(carouselId) {
+  let viewportWidth = window.innerWidth;
+
   let items = document.querySelectorAll(`#${carouselId} .carousel-item`);
+
+  let slide;  // Determine the number of slides to display
+  if (viewportWidth <= 767) {
+    slide = 2;  // 2 slides for phones
+  } else if (viewportWidth <= 1199) {
+    slide = 3;  // 3 slides for tablets
+  } else {
+    slide = 4;  // 4 slides for desktops and larger screens
+  }
+
   items.forEach((e) => {
-    const slide = 4;
     let next = e.nextElementSibling;
-    for (let i = 0; i < slide; i++) {
+    for (let i = 0; i < slide - 1; i++) {  // Clone the required number of slides
       if (!next) {
-        // wrap carousel by using first child
+        // Wrap carousel by using first child
         next = items[0];
       }
       let cloneChild = next.cloneNode(true);
